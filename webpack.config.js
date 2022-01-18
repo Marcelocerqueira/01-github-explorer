c10aee455284fcf2ab231705dfa49bdc749f6896
Marcelo cerqueira <marcelocerqueira661@gmail.com>
2022-01-18 16:58:12 -0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const path = require('path') /* aqui eu consigo recuperar o path de onde está e
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'), /*entry indica onde está o arquivo principal da aplicação.*/
-  output: { /*aqui indica onde está o arquivo de saída da aplicação.*/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'] /*como estou usando arquivos .jsx preciso falar para aplicação que é para ler arquivos .jsx*/
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'public'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    })
-  ],
-  module: {
-    rules: [ /*crio um array de regras para verificar se o arquivo é um arquivo .jsx conforme essa regra criada.*/
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'), /*entry indica onde está o arquivo principal da aplicação.*/
+    output: { /*aqui indica onde está o arquivo de saída da aplicação.*/
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'] /*como estou usando arquivos .jsx preciso falar para aplicação que é para ler arquivos .jsx*/
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [ /*crio um array de regras para verificar se o arquivo é um arquivo .jsx conforme essa regra criada.*/
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
 };
